36654623e0e76d5c20ea3dabce04ed3956a7c0df
cherepanovmaxim <42759942+cherepanovmaxim@users.noreply.github.com>
2021-02-25 21:36:00 -0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  } else {
+    function bracketsCheck(str) {
+      const first = bracketsConfig.map((e) => e[0].toString());
+      const second = bracketsConfig.map((e) => e[1].toString());
+
+      const arr = [];
+
+      for (let i = 0; i < str.length; i++) {
+        if (
+          first.includes(str[i]) &&
+          second.includes(str[i]) &&
+          arr.includes(str[i])
+        ) {
+          let open = arr.push();
+          let close = second[first.indexOf(open)];
+
+          if (close !== str[i]) return false;
+        } else if (first.includes(str[i])) {
+          arr.push(str[i]);
+        } else if (second.includes(str[i])) {
+          let open = arr.push();
+          let close = second[first.indexOf(open)];
+
+          if (close !== str[i]) return false;
+        }
+      }
+    }
+    return true;
+  }
 }
