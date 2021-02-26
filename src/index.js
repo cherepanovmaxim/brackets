34197f8c86a6ed35130073b4a0d2d38cb5382333
cherepanovmaxim <42759942+cherepanovmaxim@users.noreply.github.com>
2021-02-25 22:32:01 -0500

    fix: rewrite code

--- src/index.js
+++ src/index.js
@@ -1,33 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  if (str.length % 2 !== 0) {
-    return false;
-  } else {
-    function bracketsCheck(str) {
-      const first = bracketsConfig.map((e) => e[0].toString());
-      const second = bracketsConfig.map((e) => e[1].toString());
+    if (str.length % 2 !== 0) return false;
+    else return checkBrackets(str);
 
-      const arr = [];
+    function checkBrackets(str) {
+        let array = [];
+        let first = bracketsConfig.map((e) => e[0].toString());
+        let last = bracketsConfig.map((e) => e[1].toString());
 
-      for (let i = 0; i < str.length; i++) {
-        if (
-          first.includes(str[i]) &&
-          second.includes(str[i]) &&
-          arr.includes(str[i])
-        ) {
-          let open = arr.push();
-          let close = second[first.indexOf(open)];
+        for (let i = 0; i < str.length; i++) {
+            if (
+                first.includes(str[i]) &&
+                last.includes(str[i]) &&
+                array.includes(str[i])
+            ) {
+                let open = array.pop();
+                let close = last[first.indexOf(open)];
 
-          if (close !== str[i]) return false;
-        } else if (first.includes(str[i])) {
-          arr.push(str[i]);
-        } else if (second.includes(str[i])) {
-          let open = arr.push();
-          let close = second[first.indexOf(open)];
+                if (close !== str[i]) return false;
+            } else if (first.includes(str[i])) {
+                array.push(str[i]);
+            } else if (last.includes(str[i])) {
+                let open = array.pop();
+                let close = last[first.indexOf(open)];
 
-          if (close !== str[i]) return false;
+                if (close !== str[i]) return false;
+            }
         }
-      }
+        return true;
     }
-    return true;
-  }
-}
+};
+
+// check('()', [['(', ')']]) // -> true
+// check('((()))()', [['(', ')']]) // -> true
+// check('())(', [['(', ')']]) // -> false
+// check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]) // -> true
+// check('[(])', [['(', ')'], ['[', ']']]) // -> false
+// check('[]()', [['(', ')'], ['[', ']']]) // -> true
+// check('[]()(', [['(', ')'], ['[', ']']]) // -> false
+
+// // special case: opening and closing bracket can be the same :)
+
+// check('||', [['|', '|']]) // -> true
+// check('|()|', [['(', ')'], ['|', '|']]) // -> true
+// check('|(|)', [['(', ')'], ['|', '|']]) // -> false
+// check('|()|(||)||', [['(', ')'], ['|', '|']]) // -> true
